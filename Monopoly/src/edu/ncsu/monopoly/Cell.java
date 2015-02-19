4a26be7753ea853dbf7943d921413476ecc60f99
Paul Castles <paulcastles@hotmail.com>
2015-02-19 12:24:40 +0000

    Fourth Commit

--- Monopoly/src/edu/ncsu/monopoly/Cell.java
+++ Monopoly/src/edu/ncsu/monopoly/Cell.java
@@ -1,40 +1,72 @@
 package edu.ncsu.monopoly;
 
-public abstract class Cell {
+public abstract class Cell implements IOwnable {
 	private String name;
 	protected Player theOwner;
 	private boolean available = true;
 
+	/* (non-Javadoc)
+	 * @see edu.ncsu.monopoly.IOwnable#getName()
+	 */
+	@Override
 	public String getName() {
 		return name;
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.ncsu.monopoly.IOwnable#getOwner()
+	 */
+	@Override
 	public Player getOwner() {
 		return theOwner;
 	}
 	
+	/* (non-Javadoc)
+	 * @see edu.ncsu.monopoly.IOwnable#getPrice()
+	 */
+	@Override
 	public int getPrice() {
 		return 0;
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.ncsu.monopoly.IOwnable#playAction()
+	 */
+	@Override
 	public abstract void playAction();
 
 	void setName(String name) {
 		this.name = name;
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.ncsu.monopoly.IOwnable#setOwner(edu.ncsu.monopoly.Player)
+	 */
+	@Override
 	public void setOwner(Player owner) {
 		this.theOwner = owner;
 	}
     
-    public String toString() {
+    /* (non-Javadoc)
+	 * @see edu.ncsu.monopoly.IOwnable#toString()
+	 */
+    @Override
+	public String toString() {
         return name;
     }
 
+	/* (non-Javadoc)
+	 * @see edu.ncsu.monopoly.IOwnable#isAvailable()
+	 */
+	@Override
 	public boolean isAvailable() {
 		return available;
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.ncsu.monopoly.IOwnable#setAvailable(boolean)
+	 */
+	@Override
 	public void setAvailable(boolean available) {
 		this.available = available;
 	}
